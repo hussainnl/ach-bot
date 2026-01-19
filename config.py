131b5989f3be54847578dae95e172d31c56178b0
unknown <hussainfathy911@gmail.com>
2026-01-19 05:24:14 +0200

    fix and return the old logic

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-from google.oauth2.credentials import Credentials 
-from google.oauth2 import service_account 
+from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import os
@@ -21,15 +20,19 @@ class Configuration:
         دالة المصادقة الموحدة (تعمل مع Drive و Gmail)
         """
         try:
-            creds = service_account.Credentials.from_service_account_file(CREDS,scopes = SCOPES)
-            # if not creds or not creds.valid:
-            #     if creds and creds.expired and creds.refresh_token:
-            #         creds.refresh(Request())
-            #     else:
-            #         # تذكر حذف token.json بعد تغيير الـ SCOPES
-            #         flow = InstalledAppFlow.from_client_secrets_file(
-            #             CREDS, SCOPES)
-            #         creds = flow.run_local_server(port=0)
+            creds = None
+            if os.path.exists(TOKEN):
+                creds = Credentials.from_authorized_user_file(TOKEN, SCOPES)
+            if not creds or not creds.valid:
+                if creds and creds.expired and creds.refresh_token:
+                    creds.refresh(Request())
+                else:
+                    # تذكر حذف token.json بعد تغيير الـ SCOPES
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        CREDS, SCOPES)
+                    creds = flow.run_local_server(port=0)
+                with open(TOKEN, 'w') as token:
+                    token.write(creds.to_json())
             return creds
         except TypeError as e :
             logging.info(f"TypeError {e}")
