8f25a92bfc13a9c48f4cd6ad894fe46a0eb12df7
unknown <hussainfathy911@gmail.com>
2026-01-19 03:43:37 +0200

    add a try except

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import os
+import logging
 
 CREDS = os.getenv("CREDS")
 TOKEN = os.getenv("TOKEN")
@@ -18,17 +19,20 @@ class Configuration:
         """
         دالة المصادقة الموحدة (تعمل مع Drive و Gmail)
         """
-        creds = None
-        if os.path.exists(TOKEN):
-            creds = Credentials.from_authorized_user_file(TOKEN, SCOPES)
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                # تذكر حذف token.json بعد تغيير الـ SCOPES
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    CREDS, SCOPES)
-                creds = flow.run_local_server(port=0)
-            with open(TOKEN, 'w') as token:
-                token.write(creds.to_json())
-        return creds
+        try:
+            creds = None
+            if os.path.exists(TOKEN):
+                creds = Credentials.from_authorized_user_file(TOKEN, SCOPES)
+            if not creds or not creds.valid:
+                if creds and creds.expired and creds.refresh_token:
+                    creds.refresh(Request())
+                else:
+                    # تذكر حذف token.json بعد تغيير الـ SCOPES
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        CREDS, SCOPES)
+                    creds = flow.run_local_server(port=0)
+                with open(TOKEN, 'w') as token:
+                    token.write(creds.to_json())
+            return creds
+        except TypeError as e :
+            logging.info(f"TypeError {e}")
