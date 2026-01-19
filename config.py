a1b9a0738bbb8064bb195c9d12cb10fd6bdfc4ed
unknown <hussainfathy911@gmail.com>
2026-01-19 06:18:34 +0200

    fall test new update after delete the need for token

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
-from google.oauth2 import service_account
-
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 import os
-import logging
 
 CREDS = os.getenv("CREDS")
+TOKEN = os.getenv("TOKEN")
 SCOPE = os.getenv("SCOPES")
 SCOPES = []
 SCOPES.append(SCOPE)
@@ -17,10 +18,17 @@ class Configuration:
         """
         دالة المصادقة الموحدة (تعمل مع Drive و Gmail)
         """
-        try:
-            creds = None
-            creds = service_account.Credentials.from_service_account_file(
-                 CREDS, scopes=SCOPES)                       
-            return creds
-        except TypeError as e :
-            logging.info(f"TypeError {e}")
+        creds = None
+        if os.path.exists(TOKEN):
+            creds = Credentials.from_authorized_user_file(TOKEN, SCOPES)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                # تذكر حذف token.json بعد تغيير الـ SCOPES
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    CREDS, SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open(TOKEN, 'w') as token:
+                token.write(creds.to_json())
+        return creds
