bf0b09dbb6455b95c0761d2701159c8f88d44a64
unknown <hussainfathy911@gmail.com>
2026-01-25 02:27:04 +0200

    update the file to test the new database

--- commands/state.py
+++ commands/state.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from data_manager import Data_Manager as DB
-
+from database.user_table import User as usr
 
 import logging
 
@@ -11,12 +11,15 @@ logging.basicConfig(level=logging.INFO)
 async def state(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """To send the achievement score to the user"""
     user_id = update.effective_user.id
+    user_name =  update.effective_user.username if  update.effective_user.username else  update.effective_user.full_name
     chat_id = update.effective_chat.id
 
     # log
     logging.info(f"user_id={user_id}, chat_id={chat_id}")
+    usr().add_user(chat_id,user_id,user_name)
+    logging.info(f"add_user done")
     DB().check_user_id(user_id, chat_id)
-    user_scor = DB().state_count(user_id, chat_id)
+    user_scor = usr().get_score(user_id, chat_id)
     massage = f"✨ نقاطك : {user_scor} نقطة ✨"
     if user_scor is None:
         await update.message.reply_text("لم يتم تحديد المستخدم او المجموعة")
