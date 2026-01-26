fe91ae912a918bee1364a33652775e229dabfb79
unknown <hussainfathy911@gmail.com>
2026-01-26 07:27:08 +0200

    updated the set_rules_topic.py file to deal with mysql rather than sqlite

--- commands/set_rules_topic.py
+++ commands/set_rules_topic.py
@@ -1,14 +1,14 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-from data_manager import Bot_Setting as BS
+from database.group_table import Group
 from utils import is_admin
 async def set_rules_topic(update: Update, context: ContextTypes.DEFAULT_TYPE):    
     chat_id = update.effective_chat.id
     user_id = update.effective_user.id
     current_thread_id = update.message.message_thread_id
-    BS().add_rules_topic_id(current_thread_id, chat_id)
     if await is_admin(update, context, user_id) :
-        BS().add_rules_topic_id(current_thread_id, chat_id)
+        with Group() as Gp:
+            Gp.update_rules_topic_id(current_thread_id, chat_id)
         await update.message.reply_text("تم اعداد القوانين")
     else:
         await update.message.reply_text("ليس لديك صلاحية")
