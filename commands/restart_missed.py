7d18d11c3e996dd8ef9448eadde936c6608de551
unknown <hussainfathy911@gmail.com>
2026-01-26 07:20:05 +0200

    updated the restart_missed.py file to deal with mysql rather than sqlite

--- commands/restart_missed.py
+++ commands/restart_missed.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-from data_manager import Data_Manager as DM
+from database.user_table import User
 from utils import is_admin
 
 
@@ -9,7 +9,8 @@ async def restart_missed(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     user_id = update.effective_user.id
     if await is_admin(update, context, user_id) :
-        DM().weekly_missed_update(chat_id)
+        with User() as Ur :
+            Ur.weekly_missed_update()
         await context.bot.send_message(chat_id, text="تم عمل التشيك الأسبوعي ✅")
     else:
         await update.message.reply_text("ليس لديك صلاحية")
