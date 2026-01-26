5d900ba1bb41570b93124530e0f8b9b5d0ba9ead
unknown <hussainfathy911@gmail.com>
2026-01-26 08:17:45 +0200

    updated the set_group.py file to deal with mysql rather than sqlite

--- commands/set_group.py
+++ commands/set_group.py
@@ -10,7 +10,6 @@ async def set_group(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     group_name = update.effective_chat.title
     user_id = update.effective_user.id
     if await is_admin(update, context, user_id) :
-        BS().check_group_id(chat_id)
         with Group() as Gp:
             Gp.add_new_group(chat_id,group_name)
         await update.message.reply_text("تم اعداد المجموعة")
