6f743b2ac05c19eb350b36695ed6d451492a0075
unknown <hussainfathy911@gmail.com>
2026-02-04 09:24:01 +0200

    test the notification_topic_id

--- commands/restart_missed.py
+++ commands/restart_missed.py
@@ -8,11 +8,11 @@ from utils import is_admin
 
 async def restart_missed(update: Update, context: ContextTypes.DEFAULT_TYPE):
     group_id = update.effective_chat.id
-    user_id = update.effective_user.id
-    with Group() as Gp :
-        notification_topic_id = Gp.get_notification_topic_id(group_id)
+    user_id = update.effective_user.id        
     if await is_admin(update, context, user_id) :
-        with User() as Ur :
+        with User() as Ur ,Group() as Gp :
+            notification_topic_id = Gp.get_notification_topic_id(group_id)
+            print(notification_topic_id)
             Ur.weekly_missed_update()
         await context.bot.send_message(group_id, text="تم عمل التشيك الأسبوعي ✅",message_thread_id=notification_topic_id)
     else:
