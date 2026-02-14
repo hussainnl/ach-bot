f53f3edd9b2c0f890b9589f488881c43c96fd44d
unknown <hussainfathy911@gmail.com>
2026-02-14 03:04:28 +0200

    change the weekly check timer

--- src/commands/set_timer.py
+++ src/commands/set_timer.py
@@ -78,7 +78,7 @@ async def set_timer(application:Application):
     for group_id in  group_ids :           
         application.job_queue.run_daily(                        
             weekly_check,            
-            time= time(hour=2,minute=43,tzinfo=ZoneInfo("Africa/Cairo")),  
+            time= time(hour=3,minute=7,tzinfo=ZoneInfo("Africa/Cairo")),  
             days=(6,),  
             name=str(group_id),                   
             chat_id=group_id,
