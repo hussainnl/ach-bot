2e88203a5f69f8a60c9048aff7a96716f53cb90b
unknown <hussainfathy911@gmail.com>
2026-02-19 23:19:43 +0200

    try mongodb2

--- src/databases/mongodb/mongo_config.py
+++ src/databases/mongodb/mongo_config.py
@@ -7,5 +7,5 @@ class Config :
     def __init__(self):
         self.data = {
         "host": os.getenv("MONGO_HOST"),
-        "user": os.getenv("MONGO_USER"),
+        "username": os.getenv("MONGO_USER"),
         "password": os.getenv("MONGO_PASSWORD")}   
