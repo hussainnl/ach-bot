490ac5570b079356f4ebe187f3e3f79ebf97dd35
unknown <hussainfathy911@gmail.com>
2026-02-19 23:52:48 +0200

    try mongodb5

--- src/databases/mongodb/ach_report.py
+++ src/databases/mongodb/ach_report.py
@@ -14,17 +14,32 @@ class AchReport:
     def save_study_ach(self,user_id,group_id,group_name,text):
         """To save the user achievment study message for the weekly report"""
         database = self.db
-        database[f"ach_messages{""}"].update_one(
-        {"user_id" : user_id,
-        "group_id" : group_id,
-        "group_name" : group_name,                
-        "study_ach": [],
-        "weekly_ach": self.weekly_ach,
-        "timestamp": datetime.datetime.now(datetime.timezone.utc)
-         },
-         {"$push": {"study_ach": text}},
-         upsert=True
-        )  
+
+        if self.is_user_doc_exist(user_id,group_id) :
+                database[f"ach_messages{""}"].update_one(
+                {"user_id" : user_id,
+                "group_id" : group_id,         
+                },
+                {"$push": {"study_ach": text}}
+                )
+        else :
+            database[f"ach_messages{""}"].insert_one(
+            {"user_id" : user_id,
+            "group_id" : group_id,
+            "group_name" : group_name,                
+            "study_ach": [],
+            "weekly_ach": self.weekly_ach,
+            "timestamp": datetime.datetime.now(datetime.timezone.utc)
+             })
+          
+    def is_user_doc_exist(self,user_id,group_id) -> bool :
+        """To check if the user document exist in the database or not"""
+        database = self.db
+        doc = database[f"ach_messages{""}"].find_one({"user_id": user_id, "group_id": group_id})
+        return doc is not None
+
+
+
     def save_weekly_ach(self,user_id,group_id,text):
         """To save the user weekly ach message for the weekly report"""
         database = self.db
