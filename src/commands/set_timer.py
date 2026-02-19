210e5d12ebe5c48c0423185a4f45c0ab663b5f79
unknown <hussainfathy911@gmail.com>
2026-02-20 01:15:28 +0200

    try mongo with timer

--- src/commands/set_timer.py
+++ src/commands/set_timer.py
@@ -76,7 +76,7 @@ async def bot_timer(application:Application):
     for group_id in  group_ids :           
         application.job_queue.run_daily(                        
             weekly_check,            
-            time= time(hour=1,minute=9,tzinfo=ZoneInfo("Africa/Cairo")),  
+            time= time(hour=1,minute=18,tzinfo=ZoneInfo("Africa/Cairo")),  
             days=(5,),  
             name=str(group_id),                   
             chat_id=group_id,
--- src/databases/mongodb/ach_report.py
+++ src/databases/mongodb/ach_report.py
@@ -2,7 +2,10 @@ from databases.mongodb.mongo_config import Config
 from pymongo import MongoClient
 import os
 import datetime
+import logging
+
 db_name = os.getenv("MONGO_NAME")
+
 class AchReport:
     def __init__(self) :
         self.config = Config().data
@@ -53,16 +56,18 @@ class AchReport:
     def create_new_collection(self,collection_name) -> None:
         """To create a new collection in the database"""
         database = self.db
-        database.create_collection(collection_name)
-        self.save_crunt_collection_name(collection_name)
+        try:
+            database.create_collection(collection_name)
+        except Exception as e:
+            logging.info(f"Error creating collection {collection_name}: {e}")
+        self.save_current_collection_name(collection_name)
 
-    def save_crunt_collection_name(self,collection_name) -> None:
+    def save_current_collection_name(self,collection_name) -> None:
         """To save the name of the current collection for the new weekly report"""
         database = self.db
         database["current_collection"].update_one(
-        {"name": "collection_name",
-         "_id": "weekly_collection"},
-        {"$set": {"_id": collection_name}},
+        {"_id": "weekly_collection"},
+        {"$set": {"name": collection_name}},
         upsert=True
         )
     def get_current_collection_name(self) -> str:
