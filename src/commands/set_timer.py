54952d31518d46e22a23de6f649b9261733c0f43
unknown <hussainfathy911@gmail.com>
2026-02-21 01:11:01 +0200

    try weekly report ferture 9

--- src/commands/set_timer.py
+++ src/commands/set_timer.py
@@ -84,7 +84,7 @@ async def bot_timer(application:Application):
     for group_id in  group_ids :           
         application.job_queue.run_daily(                        
             weekly_check,            
-            time= time(hour=1,minute=8,tzinfo=ZoneInfo("Africa/Cairo")),  
+            time= time(hour=1,minute=13,tzinfo=ZoneInfo("Africa/Cairo")),  
             days=(6,),  
             name=str(group_id),                   
             chat_id=group_id,
