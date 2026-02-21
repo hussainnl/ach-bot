4222ecf9b5e335073d60f75201939a1f8263f6ce
unknown <hussainfathy911@gmail.com>
2026-02-21 03:29:00 +0200

    dubg error with logging in weekly_report command

--- src/commands/weekly_report.py
+++ src/commands/weekly_report.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram.ext import ContextTypes
 from databases.mysql.user_table import User
 from message_handler.message_handler import prepare_weekly_report
+import logging
 
 async def weekly_report(update: Update, context: ContextTypes.DEFAULT_TYPE)-> None:    
 
@@ -13,7 +14,7 @@ async def weekly_report(update: Update, context: ContextTypes.DEFAULT_TYPE)-> No
             with User() as Ur :
                 user_report = prepare_weekly_report(user_id,group_id)
                 await context.bot.send_message(user_id,user_report)
-    except:
-
+    except Exception as e:
+        logging.info(f"Error : {e}")
         await update.message.reply_text("لازم الأمر يكون في المحادثة بين وبين البوت وعندك إنجازات خلال الأسبوع ده ب الفعل")
     
