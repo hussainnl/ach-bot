bc6ecb769d796d3f25c92617e0adf156023d4c46
unknown <hussainfathy911@gmail.com>
2026-02-21 03:46:11 +0200

    add rise to /weekly_report command This command should be used in the private chat between the user and the bot

--- src/commands/weekly_report.py
+++ src/commands/weekly_report.py
@@ -5,18 +5,22 @@ from message_handler.message_handler import prepare_weekly_report
 import logging
 
 async def weekly_report(update: Update, context: ContextTypes.DEFAULT_TYPE)-> None:    
-
+    msg_group_id = update.effective_chat.id
     user_id = update.effective_user.id
     with User() as Ur:
         user_groups = Ur.is_user_a_member(user_id)
-    try:
-        for group_id in user_groups:
-            try:
-                with User() as Ur :
-                    user_report = prepare_weekly_report(user_id,group_id)
-                await context.bot.send_message(user_id,user_report)
-            except:
-                continue
+    
+    try: 
+        if msg_group_id == None:
+            raise Exception("This command should be used in the private chat between the user and the bot")
+        else:
+            for group_id in user_groups:
+                try:
+                    with User() as Ur :
+                        user_report = prepare_weekly_report(user_id,group_id)
+                    await context.bot.send_message(user_id,user_report)
+                except:
+                    continue
     except Exception as e:
         logging.info(f"Error : {e}")
         await update.message.reply_text("لازم الأمر يكون في المحادثة بين وبين البوت وعندك إنجازات خلال الأسبوع ده ب الفعل")
