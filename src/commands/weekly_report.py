81521e3525db64bfccc04703b93bd082d3675537
unknown <hussainfathy911@gmail.com>
2026-02-21 03:32:55 +0200

    dubg error with logging in weekly_report command

--- src/commands/weekly_report.py
+++ src/commands/weekly_report.py
@@ -11,9 +11,12 @@ async def weekly_report(update: Update, context: ContextTypes.DEFAULT_TYPE)-> No
         user_groups = Ur.is_user_a_member(user_id)
     try:
         for group_id in user_groups:
-            with User() as Ur :
-                user_report = prepare_weekly_report(user_id,group_id)
+            try:
+                with User() as Ur :
+                    user_report = prepare_weekly_report(user_id,group_id)
                 await context.bot.send_message(user_id,user_report)
+            except:
+                continue
     except Exception as e:
         logging.info(f"Error : {e}")
         await update.message.reply_text("لازم الأمر يكون في المحادثة بين وبين البوت وعندك إنجازات خلال الأسبوع ده ب الفعل")
